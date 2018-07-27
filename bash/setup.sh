d677d6f647f039fdecc026ba651942957812a0b3
Martin Deegan <mdeegan@umich.edu>
2018-07-27 05:23:55 -0400

    Slam/repo setup

--- bash/setup.sh
+++ bash/setup.sh
@@ -4,5 +4,24 @@ sudo apt install clang-format -y
 sudo apt install yaml-cpp -y
 sudo apt install libeigen3-dev -y
 sudo apt install libopencv-dev -y
+sudo apt install curl -y
 
 git submodule update --init
+
+cd /tmp
+git clone https://github.com/stevenlovegrove/Pangolin.git
+cd Pangolin
+mkdir build 
+cd build
+cmake ..
+make -j
+sudo make install
+
+echo('Romario please do bash')
+# TODO:
+# curl https://vision.in.tum.de/rgbd/dataset/freiburg1/rgbd_dataset_freiburg1_xyz.tgz
+# unzip this into gnc/datasets
+
+#TODO:
+# Remove Vocabulary/ORBvoc.txt
+# Unzip ORBvoc.txt.tar.gz
--- src/gnc.cpp
+++ src/gnc.cpp
@@ -1,11 +1,22 @@
 #include "gnc.hpp"
 
+using namespace cv;
+
 namespace maav {
 namespace gnc {
 
-Localizer::Localizer() {}
+Localizer::Localizer(SlamInitializer& slam_init)
+    : slam(slam_init.vocabulary_file, slam_init.config_file, slam_init.sensor,
+           slam_init.use_viewer) {}
 
-bool Localizer::is_cool() { return true; }
+Localizer::~Localizer() { slam.Shutdown(); }
+
+void Localizer::dump_image(const Mat& color, const Mat& depth,
+                           uint64_t timestamp) {
+    constexpr double MICROSECONDS_PER_SECOND = 1000000;
+    double seconds = static_cast<double>(timestamp) / MICROSECONDS_PER_SECOND;
+    slam.TrackRGBD(color, depth, seconds);
+}
 
 }  // namespace gnc
 }  // namespace maav
