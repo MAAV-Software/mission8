794c9d76a0f9f76834584798c4af18e35981957a
Martin Deegan <mdeegan@umich.edu>
2018-09-15 23:22:57 -0400

    Removed eigen install script. Updated comments from gnc setup to software setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -31,8 +31,28 @@ sudo apt install cmake \
                  libboost-test-dev -y
 
 # Install custom deps
-${SOFTWARE_DIR}/scripts/install-g++7.sh
-${SOFTWARE_DIR}/scripts/install-pangolin.sh 
-${SOFTWARE_DIR}/scripts/install-librealsense.sh
-${SOFTWARE_DIR}/scripts/install-zcm.sh
-
+# ${GNC_DIR}/bash/install-g++7.sh
+# ${GNC_DIR}/bash/install-pangolin.sh
+# ${GNC_DIR}/bash/install-librealsense.sh
+#
+# # Get TUMS dataset
+# if ! [ -f $GNC_DIR/datasets/TUMS/rgbd_dataset_freiburg1_xyz.tgz ]; then
+#     echo "Downloading TUMS dataset..."
+#     mkdir -p $GNC_DIR/datasets/TUMS
+#     curl -o $GNC_DIR/datasets/TUMS/rgbd_dataset_freiburg1_xyz.tgz \
+#     http://filecremers3.informatik.tu-muenchen.de/rgbd/dataset/freiburg1/rgbd_dataset_freiburg1_xyz.tgz
+# fi
+# echo "Extracting TUMS dataset..."
+# tar -xzf $GNC_DIR/datasets/TUMS/rgbd_dataset_freiburg1_xyz.tgz -C $GNC_DIR/datasets/TUMS
+#
+# # # Setting up ORB Vocabulary
+# echo "Setting up ORB Vocabulary..."
+# if ! [ -f $GNC_DIR/Vocabulary/ORBvoc.txt.tar.gz ]; then
+#     echo "Downloading ORB vocabulary..."
+#     mkdir -p $GNC_DIR/Vocabulary
+#     curl -o $GNC_DIR/Vocabulary/ORBvoc.txt.tar.gz \
+#     "https://doc-0k-5o-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/ls6uivmvh8fhobg79cokn2a5urb32935/1535335200000/05968329248168867851/*/1bh87Ug7bdNskxalAXprRcCSYc2YNnp2_"
+# fi
+#
+# echo "Extracting ORB vocabulatry..."
+# tar -xzf $GNC_DIR/Vocabulary/ORBvoc.txt.tar.gz -C $GNC_DIR/Vocabulary
