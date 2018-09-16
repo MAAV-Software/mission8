b8e00b1fb3c4638fe9920b818987e20eb894048d
Romario Pashollari <rpash@umich.edu>
2018-09-16 04:40:06 -0400

    Migrated GCS Client from nav

---
+++ include/gcs-client/maav-gcs-client.hpp
@@ -0,0 +1,8 @@
+/**
+* This client forwards messages from GCS to the veihice and vice versa. The GCS needs to use a udpm
+* URL while the vehicle needs to use IPC. This client is responsible for forwarding messages between
+* the two transports. NOTE: GCS needs the modified ZCM found in gcs/zcm-modified to run, but the
+* vehicle does not.
+*
+* @author Romario Pashollari (rpash@umich.edu)
+*/
