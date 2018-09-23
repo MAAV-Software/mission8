601d4d6584d26e538cbab1790ad82b99bd142a8f
dziedada <dziedada@umich.edu>
2018-09-23 07:07:11 -0400

    Changed order of installs in setup for Pangolin

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -46,10 +46,9 @@ sudo apt install cmake \
 ${SOFTWARE_DIR}/scripts/install/install-cmake.sh
 ${SOFTWARE_DIR}/scripts/install/install-g++7.sh
 ${SOFTWARE_DIR}/scripts/install/install-zcm.sh
-${SOFTWARE_DIR}/scripts/install/install-pangolin.sh
 ${SOFTWARE_DIR}/scripts/install/install-librealsense.sh
+${SOFTWARE_DIR}/scripts/install/install-pangolin.sh
 ${SOFTWARE_DIR}/scripts/install/install-pcl.sh
 ${SOFTWARE_DIR}/scripts/install/install-opencv3.sh
 ${SOFTWARE_DIR}/scripts/install/install-lcm.sh
 ${SOFTWARE_DIR}/scripts/install/install-librealsense-legacy.sh
-
