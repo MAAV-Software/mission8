f8c982e242f337614e250cfd21bbd04fa4ff474e
Martin Deegan <mdeegan@umich.edu>
2018-10-03 23:32:29 -0400

    Added script that formats everything in src and include. Also updated brace placement

---
+++ scripts/tools/format_code.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Formats all code with clang-format
+
+SOFTWARE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
+SOFTWARE_DIR="$(dirname "$GNC_DIR")"
+SRC_DIR="${SOFTWARE_DIR}/src"
+INCLUDE_DIR="${SOFTWARE_DIR}/include"
+
+# Delete Generated messages
+find ${SRC_DIR} -iname "*.cpp" | xargs clang-format -i -style=file
+find ${INCLUDE_DIR} -iname "*.hpp" | xargs clang-format -i -style=file
